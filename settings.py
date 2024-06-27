d2bf061f9ad79639dfdd136b1ab1ac5439a9d71a
thuantv503 <144916667+thuantv503@users.noreply.github.com>
2024-06-27 18:41:24 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MTE5MTAtdGh1YW50',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'related_objects',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
